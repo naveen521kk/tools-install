b4ca65db963404c4b4f2ed7d7e9bf1f68cd20bdd
Naveen M K <naveen521kk@gmail.com>
2022-07-01 13:53:05 +0000

    Config: changes
    - fix toml file
    - make sure install path exists
    - use expanduser

--- tools/config.py
+++ tools/config.py
@@ -1,8 +1,11 @@
 """Read configuration file."""
 import tomli
 from pathlib import Path
+import sys
+from utils.misc import ensure_install_path
 
 with open(Path(__file__).parent / 'config.toml', 'rb') as f:
     DATA = tomli.load(f)
 
-INSTALL_PATHS = Path(DATA['INSTALL_PATHS'])
+INSTALL_PATH = Path(DATA['INSTALL_PATHS'][sys.platform]).expanduser()
+ensure_install_path(INSTALL_PATH)
