c6cbcb6b4d4fa12e2cd41e9d994b799633ceb259
Naveen M K <naveen521kk@gmail.com>
2022-07-08 14:43:29 +0000

    Implement installing tools using `main.py`

--- main.py
+++ main.py
@@ -17,15 +17,33 @@ It's expected that each script in `tools/` directory will have a
 `install()` function that will be called for every script.
 Also, the file should contain a variable called `IS_TOOL` defined
 to `True`, only then this script will assume that the script is a tool.
-
+Mostly no code should be run when importing the file, instead should
+be in the `install()` function.
 """
+import importlib
+import os
 import sys
+from pathlib import Path
+
+from rich import print
+
+TOOLS_DIR = Path(__file__).parent / "tools"
+
+# Add TOOLS_DIR to sys.path
+sys.path.append(os.fspath(TOOLS_DIR))
 
 
 def main():
-    """
-    main entry function that can be called.
-    """
+    """Main entry function that can be called."""
+    for tool in TOOLS_DIR.iterdir():
+        if tool.is_file() and tool.name.endswith(".py"):
+            tool_name = tool.name[:-3]
+            module = importlib.import_module(tool_name)
+            if hasattr(module, "IS_TOOL") and module.IS_TOOL:
+                module.install()
+            else:
+                print(f"{tool} isn't a tool. Skipping...")
+    return 0
 
 
 if __name__ == "__main__":
