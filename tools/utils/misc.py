226d1bd133c3f220546250363722e60446af0e61
Naveen M K <naveen521kk@gmail.com>
2022-07-08 14:54:20 +0000

    Add a functiont to check if the current process is elevated

--- tools/utils/misc.py
+++ tools/utils/misc.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+import os
 from pathlib import Path
-
+import errno
 
 def get_file_name_from_url(url: str) -> str:
     return url.split("/")[-1]
@@ -11,3 +12,25 @@ def ensure_install_path(install_path: str | Path):
         install_path = Path(install_path)
     install_path.mkdir(exist_ok=True, parents=True)
     return install_path
+
+def check_if_elevated() -> bool:
+    """Check if the current process has admin privileges.
+    Returns
+    -------
+    bool
+        True if elevated else false.
+    """
+    elevated = False
+    # see https://stackoverflow.com/a/11995662/12858827
+    if os.name == 'nt':
+        p = subprocess.run(["net", "session"], capture_output=True)
+        elevated = p.returncode == 0
+    else:
+        # linux: https://stackoverflow.com/a/2806932/12858827
+        try:
+            os.rename("/etc/foo", "/etc/bar")
+            elevated = True
+        except IOError as e:
+            if e == errno.EPERM:
+                elevated = False
+    return elevated
