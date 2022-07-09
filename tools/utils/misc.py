23ae08085312f71a696f65c9d8a40f6bced0ed75
Naveen M K <naveen521kk@gmail.com>
2022-07-09 08:09:22 +0000

    Forgot a import

--- tools/utils/misc.py
+++ tools/utils/misc.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 from pathlib import Path
 import errno
+import subprocess
 
 def get_file_name_from_url(url: str) -> str:
     return url.split("/")[-1]
