664756bff90839ed963e4f58c614a6534c204eee
Naveen M K <naveen521kk@gmail.com>
2022-07-09 07:36:14 +0000

    fix: a small bug with executable handling

--- tools/install_starship.py
+++ tools/install_starship.py
@@ -65,10 +65,10 @@ def install() -> int:
             raise ValueError("Checksum mismatch. Please check if the issue is caused by cache, or update the checksum.")
         if file.name.endswith('.zip'):
             extract_zip_file(file, tmpdir)
-            shutil.copytree(tmpdir / "starship" + EXE_EXT, install_path)
+            shutil.copytree(tmpdir / ("starship" + EXE_EXT), install_path)
         else:
             extract_tar_file(file, tmpdir)
-            shutil.copy(tmpdir / "starship" + EXE_EXT, install_path)
+            shutil.copy(tmpdir / ("starship" + EXE_EXT), install_path)
         print(f"{TOOL_NAME} installed successfully.")
     return 0
 
